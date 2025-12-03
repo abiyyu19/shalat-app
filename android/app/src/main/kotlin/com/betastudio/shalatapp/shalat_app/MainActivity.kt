356f050cf7df550247cdc3f54413c7804a9d7a96
abiyyu19 <mhmdabiyyu19@gmail.com>
2025-12-03 09:50:50 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/betastudio/shalatapp/shalat_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.betastudio.shalatapp.shalat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/shalat_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShalatApp extends StatelessWidget {
+  const ShalatApp({super.key});
+
+  @override
+  Widget build(final BuildContext context) {
+    return MaterialApp();
+  }
+}
